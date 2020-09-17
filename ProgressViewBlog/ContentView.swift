c68b8899c256f7d11f80e6ed25f91a89ee2245ab
Tprezioso <tommyprezioso@gmail.com>
2020-09-16 20:17:25 -0400

    first commit

--- ProgressViewBlog/ContentView.swift
+++ ProgressViewBlog/ContentView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isProgressViewHidden = false
+    @State var progressBar = ProgressView("Loading...")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            self.isProgressViewHidden.toggle()
+        }) {
+            if isProgressViewHidden {
+                VStack {
+                    Text("Hide Progress View")
+                    progressBar
+                    
+                }
+            } else {
+                Text("Show Progress View")
+                
+            }
+        }
+    
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
